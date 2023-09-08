03c1d4cc2180f1d070aa36bd2d64acd7275989db
kyle08080000 <s106441109@gmail.com>
2023-09-08 19:00:36 +0800

    updata js

--- js/all.js
+++ js/all.js
@@ -1,132 +1,116 @@
 document.addEventListener('DOMContentLoaded', function() {
     const listGroup = document.querySelector('.list-group');
-
-    listGroup.addEventListener('change', function(event) {
-        if (event.target && event.target.matches('.form-check-input')) {
-            const checkbox = event.target;
-            const taskName = checkbox.closest('li').querySelector('.task-name');
-
-            if (checkbox.checked) {
-                taskName.classList.remove('unstriked');
-                taskName.classList.add('striked');
-            } else {
-                taskName.classList.remove('striked');
-                taskName.classList.add('unstriked');
-            }
-        }
-    });
-
-    listGroup.addEventListener('animationend', function(event) {
-        if (event.target && event.target.matches('.task-name') && event.animationName === "unstrike") {
-            event.target.classList.remove('unstriked');
-        }
-    });
-
-    // 待辦事項 list 邏輯
     const newitem = document.querySelector(".newitem");
     const addButton = document.querySelector(".addButton");
+    const sortCheckbox = document.getElementById('flexSwitchCheckDefault');
+    const clearButton = document.querySelector('.clear');
+    const pendingCountElement = document.getElementById('pending-count');
 
     let data = [
-        {
-            content: "蝦皮電商系統",
-            completed: false,
-        },
-        {
-            content: "咖啡",
-            completed: false,
-        },
-        {
-            content: "告訴 seteven 他很臭",
-            completed: false,
-        },
+        { content: "蝦皮電商系統", completed: false, originalIndex: 0 },
+        { content: "咖啡", completed: false, originalIndex: 1 },
+        { content: "告訴 steven 不要再暈船了", completed: false, originalIndex: 2 },
     ];
 
-    // 渲染資料到 HTML
+    // 渲染数据到 HTML
     function renderData() {
-        let str = "";
-        data.forEach(function (item, index) {
-            str += `<li class="list-group-item py-3 px-4 mb-1 d-flex" data-num="${index}">
-            <div class="overflow-auto me-auto">
-                <span class="task-name px-2 text-nowrap text-break ${item.completed ? 'striked' : ''}">${item.content}</span>
-            </div>
-            <div class="actions text-nowrap ms-3">
-                <input class="form-check-input me-3" type="checkbox" value="" aria-label="..." ${item.completed ? 'checked' : ''}>
-                <a href="#" class="trash">
-                    <i class="fas fa-trash"></i>
-                </a>
-            </div>
-            </li>`;
-        });
-        listGroup.innerHTML = str;
-    }
-    renderData();
-
-    // 更改完成狀態
-    listGroup.addEventListener("change", function (e) {
-        if (e.target.type === "checkbox") {
-            const parentLi = e.target.closest('li');
-            if (parentLi) {
-                const num = parentLi.getAttribute('data-num');
-                data[num].completed = e.target.checked;
-                updatePendingCount();
+        listGroup.innerHTML = '';
+        const sortedData = [...data].sort((a, b) => {
+            if (sortCheckbox.checked) {
+                return a.completed - b.completed || a.originalIndex - b.originalIndex;
             }
+            return a.originalIndex - b.originalIndex;
+        });
+        sortedData.forEach((item, index) => {
+            const listItem = document.createElement('li');
+            listItem.className = 'list-group-item py-3 px-4 mb-1 d-flex';
+            listItem.setAttribute('data-original-index', item.originalIndex);
+            listItem.innerHTML = `
+                <div class="overflow-auto me-auto">
+                    <span class="task-name px-2 text-nowrap text-break ${item.completed ? 'striked' : ''}">${item.content}</span>
+                </div>
+                <div class="actions text-nowrap ms-3">
+                    <input class="form-check-input me-3" type="checkbox" aria-label="..." ${item.completed ? 'checked' : ''}>
+                    <a href="#" class="trash">
+                        <i class="fas fa-trash"></i>
+                    </a>
+                </div>
+            `;
+            const checkbox = listItem.querySelector('.form-check-input');
+            checkbox.addEventListener('change', function() {
+                item.completed = checkbox.checked;
+                if (sortCheckbox.checked) {
+                    renderData();
+                } else {
+                    const taskName = listItem.querySelector('.task-name');
+                    taskName.classList.toggle('striked', item.completed);
+                    updatePendingCount();
+                }
+            });
+            listGroup.appendChild(listItem);
+        });
+        updatePendingCount();
+    }
+
+    // 更新待完成任务数量
+    function updatePendingCount() {
+        const pendingCount = data.filter(item => !item.completed).length;
+        pendingCountElement.textContent = '待完成項目' + pendingCount + '個';
+    }
+
+    // 更改完成状态
+    listGroup.addEventListener('change', function(e) {
+        const target = e.target;
+        if (target.matches('.trash, .trash *')) {
+            e.preventDefault();
+            const listItem = target.closest('li');
+            const originalIndex = parseInt(listItem.getAttribute('data-original-index'), 10);
+            listItem.classList.add('animate__animated', 'animate__backOutRight');
+            listItem.addEventListener('animationend', () => {
+                data = data.filter(item => item.originalIndex !== originalIndex);
+                renderData();
+            });
         }
     });
 
     // 新增待辦事項
-    addButton.addEventListener("click", function (e) {
-        if (newitem.value == "") {
+    addButton.addEventListener('click', function(e) {
+        if (newitem.value === "") {
             alert("請輸入內容");
             return;
         }
-        let obj = {};
-        obj.content = newitem.value;
-        obj.completed = false;
-        console.log(obj);
-        data.push(obj);
+        data.push({ content: newitem.value, completed: false, originalIndex: data.length });
+        newitem.value = '';
         renderData();
-        updatePendingCount();
     });
 
-    // 刪除待辦事項和更改完成狀態
-    listGroup.addEventListener("click", function (e) {
+    // 刪除待辦事項
+    listGroup.addEventListener('click', function(e) {
         const target = e.target;
-        const parentLi = target.closest('li');
-
-        if (!parentLi) return;
-
-        const index = parentLi.getAttribute('data-num');
-
-        if (target.classList.contains('form-check-input')) {
-            data[index].completed = target.checked;
-            updatePendingCount();
-        }
-
-        if (target.classList.contains('fas') && target.classList.contains('fa-trash')) {
-            data.splice(index, 1);
-            renderData();
-            updatePendingCount();
+        if (target.matches('.trash, .trash *')) {
+            e.preventDefault();
+            const listItem = target.closest('li');
+            const originalIndex = parseInt(listItem.getAttribute('data-original-index'), 10);
+            listItem.classList.add('animate__animated', 'animate__backOutRight');
+            listItem.addEventListener('animationend', () => {
+                data = data.filter(item => item.originalIndex !== originalIndex);
+                renderData();
+            });
         }
     });
 
-    // 更新待完成任務數量
-    function updatePendingCount() {
-        const pendingCount = data.filter(item => !item.completed).length;
-        document.getElementById('pending-count').textContent = '待完成項目' + pendingCount + '個';
-    }
-
-    // 初始化時調用一次，以確保正確顯示初始任務數量
-    updatePendingCount();
+    // 排序已完成項目
+    sortCheckbox.addEventListener('change', function(e) {
+        renderData();
+    });
 
     // 清除所有已完成的任務
-    document.querySelector('.clear').addEventListener('click', function (e) {
+    clearButton.addEventListener('click', function(e) {
         e.preventDefault();
         data = data.filter(item => !item.completed);
         renderData();
-        updatePendingCount();
     });
 
-
-
-
+    // 初始化时调用一次，以确保正确显示初始任务数量
+    renderData();
 });
